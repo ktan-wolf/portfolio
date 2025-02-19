0e56a9e7f33a59215642c3f25e3df546c182d898
ktan-wolf <ktan247610@gmial.com>
2025-02-19 21:55:30 +0530

    projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -50,16 +50,23 @@ const Project = () => {
             <div className={`grid  ${isMobile ? "grid-cols-2" : "grid-cols-3"} gap-6`}>
             {projects.map((project, index) => (
                 <div key={index} className="relative group overflow-hidden border rounded-lg shadow-lg">
-                <img src={project.image} alt={project.title} className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-105" />
-                <div className="absolute inset-0 flex items-center justify-center gap-4 backdrop-blur-md bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <img src={project.image} alt={project.title} className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-105" />
+                  <div className="absolute inset-0 flex items-center justify-center gap-4 backdrop-blur-md bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                      <a href={project.liveDemo} target="_blank" rel="noopener noreferrer" className="text-white text-xl bg-blue-500 p-2 rounded-full hover:bg-blue-700">
+                      <FaEye />
+                      </a>
+                      <a href={project.github} target="_blank" rel="noopener noreferrer" className="text-white text-xl bg-gray-800 p-2 rounded-full hover:bg-gray-900">
+                      <FaGithub />
+                      </a>
+                  </div>
+                  <div className='flex justify-center gap-8 pb-2 pt-2 bg-zinc-500'>
                     <a href={project.liveDemo} target="_blank" rel="noopener noreferrer" className="text-white text-xl bg-blue-500 p-2 rounded-full hover:bg-blue-700">
-                    <FaEye />
+                      <FaEye />
+                      </a>
+                      <a href={project.github} target="_blank" rel="noopener noreferrer" className="text-white text-xl bg-gray-800 p-2 rounded-full hover:bg-gray-900">
+                      <FaGithub />
                     </a>
-                    <a href={project.github} target="_blank" rel="noopener noreferrer" className="text-white text-xl bg-gray-800 p-2 rounded-full hover:bg-gray-900">
-                    <FaGithub />
-                    </a>
-                </div>
-                <h4 className="text-center text-lg font-semibold p-2 bg-gray-500 text-gray-900">{project.title}</h4>
+                  </div>
                 </div>
             ))}
             </div>
