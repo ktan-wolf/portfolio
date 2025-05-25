ab68db6f00209b57a96b0821d6d513b68e9a44d2
ktan-wolf <ktan247610@gmail.com>
2025-05-25 22:11:51 +0530

    links updated

--- src/features/profile/data/social-links.ts
+++ src/features/profile/data/social-links.ts
@@ -11,7 +11,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
     icon: "/images/link-icons/linkedin-icon.png",
     title: "LinkedIn",
     description: "ktan-wolf",
-    href: "https://www.linkedin.com/in/ktan-wolf-7b203b352/",
+    href: "https://www.linkedin.com/in/ketan-chaudhary-7b203b352/",
   },
   {
     icon: "/images/link-icons/github-icon.png",
@@ -23,6 +23,6 @@ export const SOCIAL_LINKS: SocialLink[] = [
     icon: "/images/link-icons/Resume.png",
     title: "Resume",
     description: "ktan-wolf",
-    href: "https://drive.google.com/file/d/1c8s3SZ1fsLoje82ymtjE0gcXjUAa9l-h/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1JTJdwRDP3n8vn8HFv8gc5w40InM5PxKB/view?usp=sharing",
   }
 ];
